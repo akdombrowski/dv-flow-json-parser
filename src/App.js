663d06ec045249e14b5efd3d6c0dd2c8d4cd87ba
akdombrowski <akdflyinvee73@gmail.com>
2022-05-31 06:26:26 -0500

    counts number of node types

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Button from "@mui/material/Button";
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 
+import * as _ from "lodash";
+
 hljs.registerLanguage("JSON", json);
 
 function App() {
@@ -19,6 +21,10 @@ function App() {
   const [jsonObject, setJSONObject] = React.useState("");
   const [validationError, setValidationError] = React.useState("");
   const [output, setOutput] = React.useState("");
+  const [nodes, setNodes] = React.useState("");
+  const [numOfNodes, setNumOfNodes] = React.useState("");
+  const [edges, setEdges] = React.useState("");
+  const [numOfEdges, setNumOfEdges] = React.useState("");
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -27,14 +33,20 @@ function App() {
   };
 
   const parse = () => {
-    if (!validateJSON(input)) {
+    const parsedJSON = validateJSON(input);
+    if (!parsedJSON) {
       // show error message
-      console.log("Invalid JSON`");
+      console.log("Invalid JSON");
       return;
     }
-    const parsedJSON = JSON.parse(input);
-    setOutput(parsedJSON);
-    console.log(parsedJSON);
+
+    const parsedNodes = gatherNodes(parsedJSON);
+    setNodes(parsedNodes);
+
+    const parsedEdges = gatherEdges(parsedJSON);
+    setNodes(parsedEdges);
+
+    calcNumOfEachTypeOfNode(parsedNodes, parsedEdges);
   };
 
   const jsonSyntaxHighlighting = (code) => {
@@ -48,18 +60,62 @@ function App() {
 
   const validateJSON = (input) => {
     try {
-      JSON.parse(input);
+      const parsedJSON = JSON.parse(input);
+      setOutput("parsedJSON");
+      setOutput(parsedJSON);
+      console.log(parsedJSON);
+      return parsedJSON;
     } catch (e) {
       setValidationError(e);
       console.log(e);
-      return false;
+      return null;
     }
-    return true;
+  };
+
+  const gatherNodes = (parsedJSON) => {
+    const graphData = parsedJSON.graphData;
+    const elements = graphData.elements;
+    const parsedNodes = elements.nodes;
+
+    const numberOfNodes = parsedNodes.length;
+    setNumOfNodes(numberOfNodes);
+
+    setNodes(parsedNodes);
+    return parsedNodes;
+  };
+
+  const calcNumOfEachTypeOfNode = (nodes, edges) => {
+    const nodeTypesAndCounts = {};
+
+    nodes.forEach((node) => {
+      const nodeType = node.data.nodeType;
+
+      if (!nodeTypesAndCounts[nodeType]) {
+        nodeTypesAndCounts[nodeType] = 1;
+      } else {
+        nodeTypesAndCounts[nodeType] = ++nodeTypesAndCounts[nodeType];
+      }
+
+      console.log(nodeType);
+      console.log(nodeTypesAndCounts[nodeType]);
+    });
+
+    console.log("nodeTypesAndCounts")
+    console.log(nodeTypesAndCounts)
+  };
+
+  const gatherEdges = (parsedJSON) => {
+    const graphData = parsedJSON.graphData;
+    const elements = graphData.elements;
+    const parsedEdges = elements.edges;
+
+    const numberOfEdges = parsedEdges.length;
+    setNumOfEdges(numberOfEdges);
+    return parsedEdges;
   };
 
   const parsedOutput = () => {
     if (output) {
-      
       return (
         <Grid container item xs={12} justifyContent="center" spacing={3}>
           <Grid container item xs={12}>
@@ -86,8 +142,8 @@ function App() {
             </Grid>
           </Grid>
 
-          <Grid container item xs={12}>
-            <Grid item>
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={12}>
               <TextField
                 id="outlined-textfield-connectorIds"
                 label="Connector-IDs"
@@ -96,8 +152,10 @@ function App() {
                 fullWidth
               />
             </Grid>
+          </Grid>
 
-            <Grid item>
+          <Grid container item xs={12} spacing={3}>
+            <Grid item xs={3}>
               <TextField
                 id="outlined-textfield-flowid"
                 label="Flow-ID"
@@ -105,7 +163,8 @@ function App() {
                 value={output.flowId}
               />
             </Grid>
-            <Grid item>
+
+            <Grid item xs={3}>
               <TextField
                 id="outlined-textfield-customerid"
                 label="Customer-ID"
@@ -113,20 +172,40 @@ function App() {
                 value={output.customerId}
               />
             </Grid>
-            <Grid item>
+
+            <Grid item xs={3}>
               <TextField
                 id="outlined-textfield-currentVersion"
-                label="currentVersion"
+                label="Current-Version"
                 variant="outlined"
                 value={output.currentVersion}
               />
             </Grid>
-            <Grid item>
+
+            <Grid item xs={3}>
+              <TextField
+                id="outlined-textfield-publishedVersion"
+                label="Published-Version"
+                variant="outlined"
+                value={output.publishedVersion || "null"}
+              />
+            </Grid>
+
+            <Grid item xs={3}>
+              <TextField
+                id="outlined-textfield-publishedVersion"
+                label="Input-Schema"
+                variant="outlined"
+                value={output.inputSchema || "null"}
+              />
+            </Grid>
+
+            <Grid item xs={3}>
               <TextField
                 id="outlined-textfield-publishedVersion"
                 label="publishedVersion"
                 variant="outlined"
-                value={output.publishedVersion}
+                value={output.graphData.elements}
               />
             </Grid>
           </Grid>
@@ -148,10 +227,10 @@ function App() {
           <Avatar alt="PingOne DaVinci" src={logo2} />
         </Grid>
         <Grid container item xs={12} justifyContent="center">
-          <Typography variant="h1" component="div" gutterBottom>
+          <Typography variant="h6" component="div" gutterBottom>
             PingOne DaVinci Flows
           </Typography>
-          <Typography variant="h3" component="div" gutterBottom>
+          <Typography variant="subtitle1" component="div" gutterBottom>
             JSON Export Visualizer
           </Typography>
         </Grid>
